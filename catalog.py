5e72c9c77a45644dc84e456840f71fcde134d9a4
Sir-Nova-93 <84126683+Sir-Nova-93@users.noreply.github.com>
2023-01-27 14:57:07 +0000

    Update catalog.py
    Added snowflake query for color list to a dataframe using pandas to interpret the values cleanly

--- catalog.py
+++ catalog.py
@@ -1,6 +1,7 @@
 
 import streamlit
 import snowflake.connector
+import pandas
 
 # Header section
 streamlit.title("Zena's Amazing Athleisure Catalog")
@@ -9,10 +10,14 @@ streamlit.title("Zena's Amazing Athleisure Catalog")
 #streamlit.text("Pick a sweatsuit color or style:")
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-style_list_query = my_cur.execute("Select distinct color_or_style from catalog_for_website")
-style_list = style_list_query.fetchall()
-streamlit.dataframe(style_list)
-streamlit.multiselect("Pick a sweatsuit color or style:", style_list)
+my_cur.execute("Select distinct color_or_style from catalog_for_website")
+catalog = my_cur.fetchall()
+
+df = pandas.dataframe(catalog)
+
+style_list = df[0].values.tolist()
+#streamlit.dataframe(style_list)
+streamlit.multiselect("Pick a sweatsuit color or style:", list(style_list))
 
 # Image to view selected clothing
 
