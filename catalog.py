3cd7ac125999165819eb7f9295858b53f880dfc4
Sir-Nova-93 <84126683+Sir-Nova-93@users.noreply.github.com>
2023-01-27 13:59:22 +0000

    Update catalog.py
    Updated snowflake connector and added comments for sections

--- catalog.py
+++ catalog.py
@@ -1,3 +1,23 @@
 
 import streamlit
-import snowflake
+import snowflake.connector
+
+# Header section
+streamlit.title("Zena's Amazing Athleisure Catalog")
+
+# Drop down box to select Color/Style
+
+
+# Image to view selected clothing
+
+
+# Display caption/descriotion of clothing
+
+
+# Display price
+
+
+# Display available sizes
+
+
+# Display upsell data
