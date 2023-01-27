826a4992dd3c88b52b7f85e4a015af97eef70e79
Sir-Nova-93 <84126683+Sir-Nova-93@users.noreply.github.com>
2023-01-27 15:21:37 +0000

    Update catalog.py
    Condense snowflake requery into a single call to get supporting data for selection.
    Added image and text containers for supporting information.

--- catalog.py
+++ catalog.py
@@ -19,19 +19,18 @@ style_list = df[0].values.tolist()
 #streamlit.dataframe(style_list)
 choice = streamlit.selectbox("Pick a sweatsuit color or style:", list(style_list))
 
+# Requery Snowflake to return supporting data for the selection
+my_cur.execute("Select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + choice + "' ;")
+df2 = my_cur.fetchone()
 # Image to view selected clothing
-my_cur.execute("Select direct_url from catalog_for_website where color_or_style = '" + choice + "'")
-image_url = my_cur.fetchone()
-streamlit.text(image_url)
-#streamlit.image(image_url, caption='Our warm, comfortable, ' + choice + ' sweatsuit!')
 
-# Display caption/descriotion of clothing
-
+streamlit.image(df2[0], width = 400, caption = "Our warm, comfortable, " + choice + " sweatsuit!" )
 
 # Display price
-
+streamlit.write('Price: ', df2[1])
 
 # Display available sizes
-
+streamlit.write('Sizes Available: ', df2[2])
 
 # Display upsell data
+streamlit.write(df2[3])
