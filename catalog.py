9aadb9688c15cba767077a3c0b016466377e3880
Sir-Nova-93 <84126683+Sir-Nova-93@users.noreply.github.com>
2023-01-27 14:29:33 +0000

    Update catalog.py
    Removed multiselect and display snowflake results in a dataframe to test connection

--- catalog.py
+++ catalog.py
@@ -10,8 +10,8 @@ streamlit.title("Zena's Amazing Athleisure Catalog")
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 style_list = my_cur.execute("Select distinct color_or_style from catalog_for_website")
-style_list = style_list.set_index('color_or_style')
-streamlit.multiselect("Pick a sweatsuit color or style:", list(style_list.index))
+streamlit.dataframe(style_list)
+#streamlit.multiselect("Pick a sweatsuit color or style:", list(style_list))
 
 # Image to view selected clothing
 
